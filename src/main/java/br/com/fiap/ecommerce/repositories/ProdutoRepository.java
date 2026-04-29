356c245d94795b9fd6593d34e822902b1d14bac7
Arthur <labsfiap@fiap.com.br>
2026-04-28 22:45:46 -0300

    create EstoqueService

--- src/main/java/br/com/fiap/ecommerce/repositories/ProdutoRepository.java
+++ src/main/java/br/com/fiap/ecommerce/repositories/ProdutoRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
     Optional<Produto> findByNomeProduto(String nomeProduto);
 }
--- src/main/java/br/com/fiap/ecommerce/services/EstoqueService.java
+++ src/main/java/br/com/fiap/ecommerce/services/EstoqueService.java
@@ -1,4 +1,18 @@
 package br.com.fiap.ecommerce.services;
 
+import br.com.fiap.ecommerce.entities.Estoque;
+import br.com.fiap.ecommerce.entities.Produto;
+import br.com.fiap.ecommerce.repositories.EstoqueRepository;
+import br.com.fiap.ecommerce.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EstoqueService {
+    @Autowired
+    private EstoqueRepository estoqueRepository;
+
+    public void incluirNoEstoque(Produto produto){
+        estoqueRepository.adicionarProduto(produto);
+    }
 }
