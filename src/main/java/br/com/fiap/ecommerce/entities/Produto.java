125cfe8414a481bfd4415637f46616b1fb0a4dda
Gustavo Franco <gustavo.ft2101@gmail.com>
2026-04-29 22:27:27 -0300

    CP02 - Melhoria | Normalizar entidades

--- src/main/java/br/com/fiap/ecommerce/entities/Produto.java
+++ src/main/java/br/com/fiap/ecommerce/entities/Produto.java
@@ -13,31 +13,43 @@ public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_PRODUTO")
     private Long idProduto;
 
-    @Column(nullable = false)
+    @Column(name = "NOME_PRODUTO", nullable = false, length = 150)
     private String nomeProduto;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(name = "DESCRICAO_PRODUTO", columnDefinition = "TEXT")
     private String descricaoProduto;
 
+    @Column(name = "PRECO", nullable = false)
     private Double preco;
+
+    @Column(name = "ATIVO", nullable = false)
     private Boolean ativo;
+
+    @Column(name = "PESO")
     private Double peso;
+
+    @Column(name = "ALTURA")
     private Double altura;
+
+    @Column(name = "LARGURA")
     private Double largura;
+
+    @Column(name = "PROFUNDIDADE")
     private Double profundidade;
 
     // Relacionamentos
     @ManyToOne
-    @JoinColumn(name = "id_categoria")
+    @JoinColumn(name = "ID_CATEGORIA", nullable = false)
     private Categoria categoria;
 
     @ManyToOne
-    @JoinColumn(name = "id_unidade_medida_peso")
+    @JoinColumn(name = "ID_UNIDADE_MEDIDA_PESO")
     private UnidadeMedidaPeso unidadeMedidaPeso;
 
     @ManyToOne
-    @JoinColumn(name = "id_unidade_medida_tamanho")
+    @JoinColumn(name = "UNIDADE_MEDIDA_TAMANHO")
     private UnidadeMedidaTamanho unidadeMedidaTamanho;
 }
