642a6471683fe93a72b9c5615ca11f113cd0ec86
Arthur Gomes <arthurszaa7@icloud.com>
2026-04-29 22:53:27 -0300

    fix: Padrao com Columns

--- src/main/java/br/com/fiap/ecommerce/entities/Estoque.java
+++ src/main/java/br/com/fiap/ecommerce/entities/Estoque.java
@@ -1,24 +1,26 @@
 package br.com.fiap.ecommerce.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
 @Data
 @Getter
+@Table(name = "ESTOQUE")
 public class Estoque {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_ESTOQUE")
     private Long id;
 
-    private Set<Produto> listaDeProdutos = new HashSet<>();
+    @Column(name = "NOME_SETOR", nullable = false, length = 80)
+    private String nomeSetor;
+
+    @Column(name = "PRODUTOS")
+    @OneToMany(mappedBy = "estoque")
+    private Set<Produto> produtos = new HashSet<>();
 }
