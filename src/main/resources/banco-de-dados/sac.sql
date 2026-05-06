28ac048d6a46ef3bbbf9e3ac31ed197f061cdf84
labsfiap <alecolvet12@gmail.com>
2026-05-05 22:53:22 -0300

    Atualizacao banco de dados sql SAC

--- src/main/resources/banco-de-dados/sac.sql
+++ src/main/resources/banco-de-dados/sac.sql
@@ -5,8 +5,8 @@ CREATE TABLE TB_TICKET (
     descricao        CLOB,
     status           VARCHAR2(50)  NOT NULL,
     prioridade       VARCHAR2(50)  NOT NULL,
-    dataCriacao      TIMESTAMP     DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    dataAtualizacao  TIMESTAMP     DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_criacao      TIMESTAMP     DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_atualizacao  TIMESTAMP     DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT pk_ticket          PRIMARY KEY (id),
     CONSTRAINT fk_cliente  FOREIGN KEY (id_cliente)
         REFERENCES TB_CLIENTE (id_cliente)
@@ -17,7 +17,7 @@ CREATE TABLE TB_MENSAGEM (
     id_ticket   VARCHAR2(36)       NOT NULL,
     conteudo    CLOB          NOT NULL,
     autor       VARCHAR2(200) NOT NULL,
-    dataEnvio   TIMESTAMP     DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_envio   TIMESTAMP     DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT pk_mensagem        PRIMARY KEY (id),
     CONSTRAINT fk_mensagem_ticket FOREIGN KEY (id_ticket)
         REFERENCES TICKET (id)
