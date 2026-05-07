7262b5d4faf91af6e01a81b03d8916c8e1f5da40
Arthur Gomes <arthurszaa7@icloud.com>
2026-05-06 22:54:05 -0300

    fix: Reorganizando atributos de entidade Estoque

--- src/main/java/br/com/fiap/ecommerce/repositories/EstoqueRepository.java
+++ src/main/java/br/com/fiap/ecommerce/repositories/EstoqueRepository.java
@@ -1,12 +1,15 @@
 package br.com.fiap.ecommerce.repositories;
 
 import br.com.fiap.ecommerce.entities.Estoque;
+import br.com.fiap.ecommerce.entities.Lojista;
 import br.com.fiap.ecommerce.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
-    void adicionarProduto(Long estoqueId, Produto produto);
-    void removerDoEstoque(Long estoqueId, Produto produto);
+public interface EstoqueRepository extends JpaRepository<Estoque, UUID> {
+    List<Estoque> findByResponsavelId(Long idResponsavel);
 }
