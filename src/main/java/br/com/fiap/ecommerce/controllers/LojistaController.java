617c022aeb8552ac9ad020fcaff0e1823772c61f
Fleps031 <whaleluv3r@gmail.com>
2026-05-06 22:00:23 -0300

    adding plural

--- src/main/java/br/com/fiap/ecommerce/controllers/LojistaController.java
+++ src/main/java/br/com/fiap/ecommerce/controllers/LojistaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/lojista")
+@RequestMapping("api/v1/lojistas")
 public class LojistaController {
     private final LojistaService lojistaService;
 
