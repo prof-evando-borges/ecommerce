081c35892481599c94f2a400d96240106cd2cb54
Fleps031 <whaleluv3r@gmail.com>
2026-05-06 22:01:19 -0300

    adding missing slash

--- src/main/java/br/com/fiap/ecommerce/controllers/ClienteController.java
+++ src/main/java/br/com/fiap/ecommerce/controllers/ClienteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/clientes")
+@RequestMapping("/api/v1/clientes")
 public class ClienteController {
     private final ClienteService clienteService;
 
