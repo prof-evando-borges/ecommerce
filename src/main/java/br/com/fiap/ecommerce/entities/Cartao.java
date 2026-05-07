e6a5c9748a3c937170e3f73aaa22b2321b4148e5
Luigilt20 <luigilt029@gmail.com>
2026-05-06 22:55:01 -0300

    implementa entidades Pagamento e Cartao com UUID

--- src/main/java/br/com/fiap/ecommerce/entities/Cartao.java
+++ src/main/java/br/com/fiap/ecommerce/entities/Cartao.java
@@ -8,33 +8,27 @@ import java.util.Date;
 @Table(name = "CARTAO")
 public class Cartao {
 
-    // 1º Atributo: ID como UUID (VARCHAR2 no banco)
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "ID_CARTAO", updatable = false, nullable = false, length = 36)
     private String id;
 
-    // 2º Atributo
     @NotBlank(message = "O nome do titular é obrigatório")
     @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
     @Column(name = "NOME_TITULAR", nullable = false, length = 100)
     private String nomeTitular;
 
-    // 3º Atributo
     @NotBlank(message = "O número do cartão é obrigatório")
     @Column(name = "NUMERO", nullable = false, length = 16)
     private String numero;
 
-    // 4º Atributo
     @NotBlank(message = "O CVV é obrigatório")
     @Column(name = "CVV", nullable = false, length = 3)
     private String cvv;
 
-    // 5º Atributo
     @NotNull(message = "A data de criação deve ser válida")
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "DATA_CRIACAO", updatable = false)
     private Date dataCriacao;
 
-    // Lembre-se de gerar os Getters e Setters
 }
--- src/main/java/br/com/fiap/ecommerce/entities/Pagamento.java
+++ src/main/java/br/com/fiap/ecommerce/entities/Pagamento.java
@@ -9,33 +9,27 @@ import java.util.Date;
 @Table(name = "PAGAMENTO")
 public class Pagamento {
 
-    // 1º Atributo: ID como UUID (VARCHAR2 no banco)
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "ID_PAGAMENTO", updatable = false, nullable = false, length = 36)
     private String id;
 
-    // 2º Atributo
     @NotNull(message = "O valor é obrigatório")
     @DecimalMin(value = "0.1", message = "O valor deve ser maior que zero")
     @Column(name = "VALOR", nullable = false)
     private BigDecimal valor;
 
-    // 3º Atributo
     @NotBlank(message = "O status do pagamento é obrigatório")
     @Column(name = "STATUS_PAGAMENTO", nullable = false, length = 50)
     private String statusPagamento;
 
-    // 4º Atributo
     @NotNull(message = "A data de criação deve ser válida")
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "DATA_CRIACAO", updatable = false)
     private Date dataCriacao;
 
-    // 5º Atributo: Relacionamento (Foreign Key que o professor desenhou)
     @ManyToOne
     @JoinColumn(name = "ID_CARTAO", nullable = false)
     private Cartao cartao;
 
-    // Lembre-se de gerar os Getters e Setters
 }
