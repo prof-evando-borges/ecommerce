1693bb5a73bf3c5e668af861ffad389f7823e49d
kaua <rodrigueskaua76@gmail.com>
2026-05-12 22:56:26 -0300

    CORRECTIONS OF MALENDER  UUID

--- src/main/java/br/com/fiap/ecommerce/repositories/AvaliacaoRepository.java
+++ src/main/java/br/com/fiap/ecommerce/repositories/AvaliacaoRepository.java
@@ -2,10 +2,11 @@ package br.com.fiap.ecommerce.repositories;
 
 import br.com.fiap.ecommerce.entities.Avaliacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
-
+@Repository
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, UUID> {
 
     Optional<Avaliacao> findByCliente_IdAndProduto_Id(
--- src/main/java/br/com/fiap/ecommerce/repositories/ComentarioRepository.java
+++ src/main/java/br/com/fiap/ecommerce/repositories/ComentarioRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
-
+@Repository
 public interface ComentarioRepository extends JpaRepository<Comentario, UUID> {
 
     // Buscar comentários por avaliação
