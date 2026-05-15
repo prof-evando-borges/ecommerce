b96687a12da3b48749c40f9beacf4805dc425a74
Leo Queijo <leoqueijo8@hotmail.com>
2026-05-15 20:11:18 -0300

    Commit da sexta 20hr

--- src/main/java/br/com/fiap/ecommerce/services/PedidoService.java
+++ src/main/java/br/com/fiap/ecommerce/services/PedidoService.java
@@ -2,15 +2,14 @@ package br.com.fiap.ecommerce.services;
 
 import br.com.fiap.ecommerce.entities.Pedido;
 import br.com.fiap.ecommerce.repositories.PedidoRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class PedidoService {
     @Autowired
     private final PedidoRepository repository;
@@ -20,20 +19,24 @@ public class PedidoService {
     }
 
     @Transactional
-    public Pedido salvar(Pedido pedido) throws InterruptedException {
-        if ((pedido.getNumeroPedido() == null) || pedido.getNumeroPedido().wait().isEmpty()) {
+    public Pedido salvar(Pedido pedido) {
+
+        if (pedido.getNumeroPedido() == null || pedido.getNumeroPedido() <= 0) {
             throw new IllegalArgumentException("O número do pedido é obrigatório.");
         }
 
         Optional<Pedido> existente = repository.findById(pedido.getNumeroPedido());
-        if (existente.isPresent() && !existente.get().getNumeroPedido().equals(pedido.getNumeroPedido())) {
-            throw new IllegalArgumentException("Já existe um pedido cadastrada com este numero.");
+
+        if (existente.isPresent() &&
+                !existente.get().getNumeroPedido().equals(pedido.getNumeroPedido())) {
+
+            throw new IllegalArgumentException("Já existe um pedido cadastrado com este número.");
         }
 
         return repository.save(pedido);
     }
 
-    public Pedido buscarPorId(Long id) {
+    public Pedido buscarPorId(UUID id) {
         Pedido pedido = (Pedido) repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Pedido não encontrado com o número: " + id));
         return pedido;
